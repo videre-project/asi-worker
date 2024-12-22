d29b97f2c38bb409d3c4f9d4dcbaff88a31bb402
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2024-12-21 21:33:41 -0600

    Create response handler, add JSON validation
    Removes fastapi for a pure-python API router.

---
+++ src/router.py
@@ -0,0 +1,43 @@
+## @file
+# Copyright (c) 2024, Cory Bennett. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+##
+"""Cloudflare workers API router and FFI utilities."""
+
+from js import Response, Headers
+import json
+
+
+class Router:
+  """Simple router for handling request routes."""
+  _routes = {}
+
+  def __getattr__(self, method):
+    def decorator(path):
+      def wrapper(f):
+        self._routes[(method.upper(), path)] = f
+        return f
+      return wrapper
+    return decorator
+
+  def match(self, method, path):
+    return self._routes.get((method.upper(), path))
+
+def JSONResponse(data: dict, status: int = 200) -> Response:
+  headers = Headers.new({"content-type": "application/json"}.items())
+  return Response.new(json.dumps(data), headers=headers, status=status)
+
+def get_endpoint(url: str) -> str:
+  """Extract the endpoint from the request url string."""
+  return '/' + url.split('/', 3)[-1].split('?')[0]
+
+def get_parameters(url: str) -> dict[str, str]:
+  """Extract the query parameters from the request url string."""
+  parameters: dict = {}
+  query: str = url.split('?')[1]
+  for param in query.split('&'):
+    key = param.split('=')[0]
+    value = param.split('=')[1]
+    parameters[key] = value
+
+  return parameters
--- src/worker.py
+++ src/worker.py
@@ -1,20 +1,48 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+## @file
+# Copyright (c) 2024, Cory Bennett. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+##
+"""Cloudflare worker script for calculating the nearest archetypes with ASI."""
+
+from pyodide.ffi import JsArray
 
 from asi import find_nearest_archetypes
 from artifacts import *
 
-async def on_fetch(request, env):
-  # pylint: disable=import-error
-  import asgi
-  return await asgi.fetch(app, request, env)
+from router import Router, JSONResponse, get_endpoint, get_parameters
 
-app = FastAPI()
 
-@app.post("/")
-async def get_scores(request: list[str], format: str):
+api = Router()
+
+@api.post('/')
+async def index(request, params, env):
+  try:
+    cards = await request.json()
+    # The returned card object will be a `JsProxy` object, which we'll need to
+    # probe to check the kind of JSON object returned to us.
+    assert type(cards).__name__ == "JsProxy"
+    assert cards.typeof == "object"
+  except:
+    return JSONResponse({
+      "error": "Invalid JSON",
+      "message": "The request body must be a valid JSON object."
+    }, status=400)
+  else:
+    if not isinstance(cards, JsArray):
+      return JSONResponse({
+        "error": "Invalid JSON",
+        "message": "The request body must be a valid JSON array. " +
+                  f"to_py: {cards.to_py}" +
+                  f"Got type '{str(dir(cards))}'."
+      }, status=400)
+    elif cards.length < 2:
+      return JSONResponse({
+        "error": "Invalid JSON",
+        "message": "The request body must contain at least two cards."
+      }, status=400)
+
   bigrams: dict[str, int] = None
-  match format:
+  match (format := params.get("format", None)):
     case "standard":
       bigrams = standard_bigrams
     case "modern":
@@ -28,7 +56,28 @@ async def get_scores(request: list[str], format: str):
     case "pauper":
       bigrams = pauper_bigrams
     case _:
-      return JSONResponse(content={"error": "Invalid format"}, status_code=400)
+      if format is None or len(format) == 0:
+        return JSONResponse({
+          "error": "Missing parameter",
+          "message": "The 'format' parameter is required."
+        }, status=400)
 
-  scores = find_nearest_archetypes(bigrams, request)
-  return JSONResponse(content={ k: round(v, 4) for k, v in scores.items() })
+      return JSONResponse({
+        "error": "Invalid format",
+        "message": f"The 'format' parameter '{format}' is not supported."
+      }, status=400)
+
+  return JSONResponse(find_nearest_archetypes(bigrams, cards))
+
+async def on_fetch(request, env):
+  method: str = request.method
+  endpoint = get_endpoint(request.url)
+  params = get_parameters(request.url)
+
+  if (handler := api.match(method, endpoint)):
+    return await handler(request, params, env)
+  else:
+    return JSONResponse({
+      'error': 'Invalid request method',
+      'message': 'Did not match any request route handlers.'
+    }, status=405)
